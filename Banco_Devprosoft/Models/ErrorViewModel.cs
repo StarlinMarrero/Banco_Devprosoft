2d50b6c562f587e8d7bc21082130efb7f5c528c2
Starlin Marrero <starlin.marrero@hotmail.com>
2021-03-20 20:41:07 -0400

    Inicial

---
+++ Banco_Devprosoft/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Banco_Devprosoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
