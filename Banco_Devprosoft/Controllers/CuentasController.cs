837a6339c23ee2ba18617c02d4f62b2cbbf94c78
Aneury02 <capellananeury@gmail.com>
2021-03-24 00:49:12 -0400

    Implementacion del formulario de solicitud cuentas, solo frontEnd.

--- Banco_Devprosoft/Controllers/CuentasController.cs
+++ Banco_Devprosoft/Controllers/CuentasController.cs
@@ -1,4 +1,5 @@
 ﻿using Banco_Devprosoft.Models;
+using Banco_Devprosoft.Data;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,13 +8,32 @@ using System.Threading.Tasks;
 
 namespace Banco_Devprosoft.Controllers
 {
+  
     public class CuentasController : Controller
     {
+        private ApplicationDbContext db;
+
+        public CuentasController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        
+        public IActionResult Solicitar_Cuenta()
+        {
+            return View();
+        }
+
+        public JsonResult add_Solicitud_Cuentas(Solicitud_Cuenta model)
+        {
+            var validacion = db.Solicitudes_Cuentas.Where(p => p.Cedula == model.Cedula).FirstOrDefault();
+            return Json(new { title = "Solicitud de Cuentas", text = "Su Solicitud ha sido enviada", icon = "success"});
+        }
+
+
     }
 }
