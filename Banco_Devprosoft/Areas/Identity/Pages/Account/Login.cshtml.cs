70b21e9af5f700fa633f87ac2fb2988650fb03e2
Aneury02 <capellananeury@gmail.com>
2021-03-23 22:51:26 -0400

    Diseno de la vista home

--- Banco_Devprosoft/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Banco_Devprosoft/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,8 +44,7 @@ namespace Banco_Devprosoft.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            public string UserName { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -82,7 +81,7 @@ namespace Banco_Devprosoft.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
