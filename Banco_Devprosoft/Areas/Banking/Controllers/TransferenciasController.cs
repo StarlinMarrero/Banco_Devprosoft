258baea3b5d292cd1b080562e3db27b6e12e97a5
ydanielsb97 <ydanielsb97@gmail.com>
2021-03-29 01:09:36 -0400

    Transferencia al 100%

--- Banco_Devprosoft/Areas/Banking/Controllers/TransferenciasController.cs
+++ Banco_Devprosoft/Areas/Banking/Controllers/TransferenciasController.cs
@@ -1,4 +1,5 @@
 ﻿using Banco_Devprosoft.Data;
+using Banco_Devprosoft.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -25,27 +26,59 @@ namespace Banco_Devprosoft.Areas.Banking.Controllers
             return View();
         }
 
+        public JsonResult Verificar_Transferencia(int Cuenta_Destino, int Monto)
+        {
+            var UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+           
+
+            var cuenta = db.Cuentas_Bancarias.Where(x => x.Propietario_ID == UserId).FirstOrDefault();
+
+
+            var Validation_Destino = db.Cuentas_Bancarias.Find(Cuenta_Destino);
+
+
+            if(Validation_Destino == null)
+            {
+                return Json(new { title = "Transferencias", text = "La cuenta no fué encontrada", icon = "error" });
+
+            }
+
+            if (Monto > cuenta.Balance)
+            {
+                return Json(new { title = "Transferencias", text = "Su cuenta posee menos balance del solicitado a transferir.", icon = "error" });
+
+            }
+            var user = db.Users.Find(Validation_Destino.Propietario_ID);
+
+            var Nombre_Completo = $"{user.Nombres} {user.Apellidos}";
+            return Json(new { Nombre_Completo });
+        }
+
         public JsonResult Realizar_Transferencia(int Cuenta_Destino, int Monto)
         {
             var UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            var cuenta = db.Cuentas_Bancarias.Where(x => x.Propietario_ID == UserId).FirstOrDefault();
+            var cuenta_Origen = db.Cuentas_Bancarias.Where(x => x.Propietario_ID == UserId).FirstOrDefault();
 
-            var Validation_Destino = db.Cuentas_Bancarias.Find(Cuenta_Destino);
+            var cuenta_Destino = db.Cuentas_Bancarias.Find(Cuenta_Destino);
 
-            if(Validation_Destino == null)
+            cuenta_Origen.Balance = cuenta_Origen.Balance - Monto;
+            cuenta_Destino.Balance = cuenta_Destino.Balance + Monto;
+
+            var model = new Transferencia
             {
-                return Json(new {  });
+                Cuenta_Destino_ID = Cuenta_Destino,
+                Cuenta_Origen_ID = cuenta_Origen.Cuenta_ID,
+                Fecha_Transaccion = DateTime.Now,
+                Monto = Monto
 
-            }
+            };
 
-            if (Monto >= cuenta.Balance)
-            {
-                return Json("");
+            db.Transferencias.Add(model);
+            db.SaveChanges();
 
-            }
-
-            return Json("");
+            return Json(new { title = "Transferencias", text = "Transferencia realizada exitósamente", icon = "success" });
         }
 
     }
